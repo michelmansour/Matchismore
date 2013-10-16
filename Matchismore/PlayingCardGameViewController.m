cf027a38f2f2f7f8fbba86db95294f8b74a96e4c
Michel Mansour <michel.mansour@gmail.com>
2013-10-15 21:52:03 -0400

    pushed updating of flip description to subclasses; parameterized whether matched cards should be removed from play

--- Matchismore/PlayingCardGameViewController.m
+++ Matchismore/PlayingCardGameViewController.m
@@ -13,6 +13,7 @@
 
 @interface PlayingCardGameViewController ()
 @property (nonatomic) NSUInteger deckStartSize;
+@property (weak, nonatomic) IBOutlet UITextView *descText;
 @end
 
 #define DEFAULT_NUM_CARDS_IN_PLAY 22
@@ -27,6 +28,10 @@
     // nothing yet...
 }
 
+- (BOOL)removeUnplayableCards {
+    return NO;
+}
+
 - (NSUInteger)deckStartSize {
     if (_deckStartSize) {
         return _deckStartSize;
@@ -38,8 +43,12 @@
     return [[PlayingCardDeck alloc] init];
 }
 
-- (NSUInteger)matchSetSizeToPlayWith {
+- (NSUInteger)matchSetSize {
     return 2;
+}
+
+- (NSUInteger)flipCost {
+    return 1;
 }
 
 - (NSAttributedString *)displayStringForCard:(Card *)card {
@@ -69,6 +78,58 @@
             pccvc.playingCardView.alpha = playingCard.isUnplayable ? 0.3 : 1.0;
         }
     }
+}
+
+
+- (NSAttributedString *)joinAttrStrings:(NSArray *)strs withString:(NSString *)delim {
+    NSMutableAttributedString *result = [[NSMutableAttributedString alloc] init];
+    
+    NSAttributedString *delimiter = [[NSAttributedString alloc] initWithString:delim];
+    for (id item in strs) {
+        if ([item isKindOfClass:[NSAttributedString class]]) {
+            NSAttributedString *str = (NSAttributedString *)item;
+            if (result.length > 0) {
+                [result appendAttributedString:delimiter];
+            }
+            [result appendAttributedString:str];
+        }
+    }
+    
+    return result;
+}
+
+- (void)updateUI {
+    [super updateUI];
+    
+    // set the last flip description
+    NSMutableAttributedString *descStr = [[NSMutableAttributedString alloc] initWithString:@""];
+    if (self.game.lastFlipWasMatchCheck) {
+        NSMutableArray *cardContents = [[NSMutableArray alloc] init];
+        [cardContents addObject:[self displayStringForCard:self.game.lastFlipCard]];
+        for (id item in self.game.lastCardsChecked) {
+            if ([item isKindOfClass:[Card class]]) {
+                Card *matchCard = (Card *)item;
+                [cardContents addObject:[self displayStringForCard:matchCard]];
+            }
+        }
+        if (self.game.lastFlipWasMatch) {
+            [descStr appendAttributedString:[[NSAttributedString alloc] initWithString:@"Matched "]];
+            [descStr appendAttributedString:[self joinAttrStrings:cardContents withString:@"&"]];
+            [descStr appendAttributedString:[[NSAttributedString alloc] initWithString:[[NSString alloc] initWithFormat:@" for %d points", self.game.lastFlipScore]]];
+        } else {
+            [descStr appendAttributedString:[self joinAttrStrings:cardContents withString:@"&"]];
+            [descStr appendAttributedString:[[NSAttributedString alloc] initWithString:[[NSString alloc] initWithFormat:@" don't match! %d point penalty!", -1 * self.game.lastFlipScore]]];
+        }
+    } else if (self.game.lastFlipCard) {
+        if (self.game.lastFlipCard.isFaceUp) {
+            [descStr appendAttributedString:[[NSAttributedString alloc] initWithString:@"Flipped up "]];
+            [descStr appendAttributedString:[self displayStringForCard:self.game.lastFlipCard]];
+        } else {
+            [descStr appendAttributedString:[[NSAttributedString alloc] initWithString:@"Flipped down "]];
+            [descStr appendAttributedString:[self displayStringForCard:self.game.lastFlipCard]];
+        }
+    }
+    self.descText.attributedText = descStr;
 }
 
 @end
